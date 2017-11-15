51490b1d0d5cb17459669aafd1f22a7f95a12f91
illia.yanovoy <illia.yanovoy@nuweb.com>
2017-11-15 18:33:02 +0200

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserService } from "./user.service";
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './messages/message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { MessageService } from './messages/message.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [UserService, MessageService],
   bootstrap: [AppComponent]
